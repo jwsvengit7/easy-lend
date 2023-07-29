de92be6016731286cb084f69e69376a9946b3cb1
edirin nelson <nelsonedirin@gmail.com>
2023-07-30 00:04:34 +0100

    some implementations fixed

--- profile-management-service/src/main/java/com/decagon/service/serviceImplementation/ProfileServiceImpl.java
+++ profile-management-service/src/main/java/com/decagon/service/serviceImplementation/ProfileServiceImpl.java
@@ -57,38 +57,21 @@ public class ProfileServiceImpl implements ProfileService {
     @Override
     public ProfileResponseDTO updateEmploymentStatus(EmploymentStatusDTO employmentStatusDTO, String authorizationHeader) {
         String userId = getUserID(authorizationHeader);
-
-        // Fetch the profile from the database using the provided userId
         Profile profile = profileRepository.findByUserId(userId)
                 .orElseThrow(() -> new ProfileNotFoundException("Profile not found for user ID: " + userId));
-
-        // Convert EmploymentStatusDTO to EmploymentStatus
         EmploymentStatus employmentStatus = new EmploymentStatus(employmentStatusDTO);
-
-        // Update the profile with the new EmploymentStatus
         profile.setEmploymentStatus(employmentStatus);
-
-        // Save the updated profile in the database
         profileRepository.save(profile);
-
-        // Prepare the response DTO
-
         return new ProfileResponseDTO(profile);
     }
 
     @Override
     public ProfileResponseDTO updateGovernmentID(GovernmentIDDTO governmentIDDTO, MultipartFile file, String authorizationHeader) {
         String userId = getUserID(authorizationHeader);
-
-        // Fetch the profile from the database using the provided userId
         Profile profile = profileRepository.findByUserId(userId)
                 .orElseThrow(() -> new ProfileNotFoundException("Profile not found for user ID: " + userId));
-
         String url = uploadFile(file, profile.getId());
-        // Update the GovernmentID screen
         GovernmentID governmentID = new GovernmentID(governmentIDDTO);
-
-        // Update the profileCreationStatus to reflect the screen update
         profile.setGovernmentId(governmentID);
         profile.setStatus(ProfileStatus.GOVERNMENT_UPDATED);
         profileRepository.save(profile);
@@ -98,57 +81,37 @@ public class ProfileServiceImpl implements ProfileService {
     @Override
     public ProfileResponseDTO updateIncomeStatus(IncomeStatusDTO incomeStatusDTO, String authorizationHeader) {
         String userId = getUserID(authorizationHeader);
-
-        // Fetch the profile from the database using the provided userId
         Profile profile = profileRepository.findByUserId(userId)
                 .orElseThrow(() -> new ProfileNotFoundException("Profile not found for user ID: " + userId));
-
-        // Update the IncomeStatus screen
         IncomeStatus incomeStatus = new IncomeStatus(incomeStatusDTO);
         profile.setIncomeStatus(incomeStatus);
-        // Update the profileCreationStatus to reflect the screen update
         profile.setStatus(ProfileStatus.INCOME_UPDATED);
-
         profileRepository.save(profile);
-
         return new ProfileResponseDTO(profile);
     }
 
     @Override
     public ProfileResponseDTO updateBankAccount(BankAccountDTO bankAccountDTO, String authorizationHeader) {
         String userId = getUserID(authorizationHeader);
-
-        // Fetch the profile from the database using the provided userId
         Profile profile = profileRepository.findByUserId(userId)
                 .orElseThrow(() -> new ProfileNotFoundException("Profile not found for user ID: " + userId));
-
-        // Update the BankAccount screen
         BankAccount bankAccount = new BankAccount(bankAccountDTO);
         profile.setBankAccount(bankAccount);
-        // Update the profileCreationStatus to reflect the screen update
         profile.setStatus(ProfileStatus.BANK_ACCOUNT_UPDATED);
-
         profileRepository.save(profile);
-
         return new ProfileResponseDTO(profile);
     }
 
     @Override
     public ProfileResponseDTO updateProofOfAddress(ProofOfAddressDTO proofOfAddressDTO, MultipartFile file, String authorizationHeader) {
         String userId = getUserID(authorizationHeader);
-
-        // Fetch the profile from the database using the provided userId
         Profile profile = profileRepository.findByUserId(userId)
                 .orElseThrow(() -> new ProfileNotFoundException("Profile not found for user ID: " + userId));
-
         String url = uploadFile(file, profile.getId());
         ProofOfAddress proofOfAddress = new ProofOfAddress(proofOfAddressDTO);
         profile.setProofOfAddress(proofOfAddress);
-        // Update the profileCreationStatus to reflect the screen update
         profile.setStatus(ProfileStatus.PROOF_OF_ADDRESS);
-
         profileRepository.save(profile);
-
         return new ProfileResponseDTO(profile);
     }
 
