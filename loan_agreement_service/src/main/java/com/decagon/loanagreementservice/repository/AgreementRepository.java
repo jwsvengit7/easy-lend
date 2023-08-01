c8fd3590cd6ed9e1e01e613d8160d6a5e0e8afa3
Nsonwu Chinwendu <nsonwuchiwendu@gmail.com>
2023-08-01 08:34:14 +0100

    Amended the Transaction Service Call

--- loan_agreement_service/src/main/java/com/decagon/loanagreementservice/repository/AgreementRepository.java
+++ loan_agreement_service/src/main/java/com/decagon/loanagreementservice/repository/AgreementRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface AgreementRepository extends JpaRepository<LoanAgreement, Long> {
+public interface AgreementRepository extends JpaRepository<LoanAgreement, String> {
     Optional<LoanAgreement> findByLoanId(String loanId);
 }
--- loan_agreement_service/src/main/java/com/decagon/loanagreementservice/services/BorrowerService.java
+++ loan_agreement_service/src/main/java/com/decagon/loanagreementservice/services/BorrowerService.java
@@ -1,6 +1,6 @@
 package com.decagon.loanagreementservice.services;
 
-import com.decagon.loanagreementservice.dtos.request.LoanAgreementDto;
+import com.decagon.loanagreementservice.dtos.response.LoanAgreementDto;
 import com.decagon.loanagreementservice.models.LoanOffer;
 import jakarta.servlet.http.HttpServletRequest;
 
